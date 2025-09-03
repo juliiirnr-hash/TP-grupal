e70957bf487091895db89ab09288cd5ea743e2e6
4Angy <angyzalazar18@gmail.com>
2025-09-03 17:24:03 -0300

    Add files via upload
    Agrego ejercicios 13 y 14

---
+++ Trabajo Grupal 1/ejercicio13.py
@@ -0,0 +1,7 @@
+cadena1 = input("A continuacion ingrese una cadena de texto: ")
+cadena2 = input("A continuacion ingrese otra cadena de texto: ")
+
+if cadena2 in cadena1:
+    print("La 2da cadena pertenece a la primera")
+else:
+    print("La 2da cadena no pertenece a la primera")
